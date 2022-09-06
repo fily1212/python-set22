87c88487219cf0b061964f82cd1bf199921e491c
fily1212 <fily1212@gmail.com>
2022-09-06 12:33:22 +0200

    es min

--- esempi_for.py
+++ esempi_for.py
@@ -5,5 +5,16 @@ min, massimo, media
 num = int(input("Quanti voti vuoi inserire? "))
 voti = []
 for i in range(num):
+    print(i)
     voti.append(int(input("Voto " + str(i+1) + ": ")))
 print(voti)
+
+min = voti[0]
+for voto in voti:
+    if voto < min:
+        min = voto
+print(min)
+
+for lettera in 'CIAO':
+    print(lettera)
+# stampare le vocali del proprio nome
