ba4deeac39bd7ad7755b1631501b33f1a57170c7
fily1212 <fily1212@gmail.com>
2022-09-06 12:10:39 +0200

    for

--- esempi_for.py
+++ esempi_for.py
@@ -2,3 +2,8 @@
 salvarli in una lista e calcolare: 
 min, massimo, media
 ''' 
+num = int(input("Quanti voti vuoi inserire? "))
+voti = []
+for i in range(num):
+    voti.append(int(input("Voto " + str(i+1) + ": ")))
+print(voti)
