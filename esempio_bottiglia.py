6fceef3bfc940d9e025a0dd39710ed933887bb49
fily1212 <fily1212@gmail.com>
2022-09-08 10:57:33 +0200

    classi e sottoclassi

--- esempio_bottiglia.py
+++ esempio_bottiglia.py
@@ -22,9 +22,42 @@ class Bottiglia:
         if q > 0:
             self.q = max(self.q - q, 0)
 
+# Una sottoclasse è una classe che eredità le proprietà della classe genitore
+class BottigliaConTappo(Bottiglia):
+    def __init__(self, q, c):
+        self.aperta = True
+        super().__init__(q, c)
+    
+    def riempi(self, q):
+        if self.aperta:
+            super().riempi(q)
+
+    def svuota(self, q):
+        if self.aperta:
+            super().svuota(q)
+
+    def apri(self):
+        self.aperta = True
+    
+    def chiudi(self):
+        self.aperta = False
+
+    def __str__(self):
+        return "Bottiglia - capacità: " + str(self.c) + \
+        " - quantità: " + str(self.q) + " aperta: " + str(self.aperta)
+        
+
 a = Bottiglia(750,500)
 print(a)
 a.riempi(1000)
 print(a)
 a.svuota(2200)
 print(a)
+b = BottigliaConTappo(1000, 500)
+print(b)
+b.svuota(100)
+print(b)
+b.chiudi()
+print(b)
+b.svuota(100)
+print(b)
